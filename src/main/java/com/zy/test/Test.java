75f23c1e43966df43c474d47a94ccda600cca8d2
zy <zhongyuan966@gmail.com>
2018-09-08 15:32:46 +0800

    新增test.java

--- src/main/java/com/zy/test/Test.java
+++ src/main/java/com/zy/test/Test.java
@@ -5,6 +5,7 @@ public class Test {
     public static void main(String[] args) {
         System.out.print("Hello world");
         test();
+        test();
     }
 
     public static void test() {
