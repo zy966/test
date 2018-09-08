932d89b560d4c92bcbc6820d4edae4275233f571
zy <zhongyuan966@gmail.com>
2018-09-08 15:28:12 +0800

    新增test.java

--- src/main/java/com/zy/executors/ScheduledThreadTest.java
+++ src/main/java/com/zy/executors/ScheduledThreadTest.java
@@ -9,48 +9,49 @@ import java.util.concurrent.TimeUnit;
 
 public class ScheduledThreadTest {
 
-	public static void main(String[] args) throws Exception {
-		ScheduledExecutorService executor = Executors.newScheduledThreadPool(5);
-		
-		Map<Integer, ScheduledFuture<?>> futures = new HashMap<Integer, ScheduledFuture<?>>();
-		
-		for(int i=0; i<5; i++) {
-			final int index  = i;
-			ScheduledFuture<?> sf = executor.scheduleAtFixedRate(new Runnable() {
-				int number = index;
-				@Override
-				public void run() {
-					System.out.println(number);
-					try {
-						Thread.sleep(1000);
-					} catch (InterruptedException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-				}
-				
-			}, 1, 1, TimeUnit.SECONDS);
-			
-			futures.put(i, sf);
-		}
+    public static void main(String[] args) throws Exception {
+        ScheduledExecutorService executor = Executors.newScheduledThreadPool(5);
 
-		Thread.sleep(5000);
-		
-		for(int i=0; i<5; i++) {
-			ScheduledFuture<?> sf = futures.get(i);
-			if(sf.isCancelled()) 
-				System.out.println("thread " + i + " is cancelled");
-			else {
-				if(sf.cancel(true)) 
-					System.out.println("thread " + i + " is cancelled right now");
-				else
-					System.out.println("thread " + i + " is fail to cancel");
-			}
-			
-			Thread.sleep(2000);
-		}
-		
-		executor.shutdown();
-	}
+        Map<Integer, ScheduledFuture<?>> futures = new HashMap<>();
+
+        for (int i = 0; i < 5; i++) {
+            final int index = i;
+            ScheduledFuture<?> sf = executor.scheduleAtFixedRate(new Runnable() {
+                int number = index;
+
+                @Override
+                public void run() {
+                    System.out.println(number);
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+                }
+
+            }, 1, 1, TimeUnit.SECONDS);
+
+            futures.put(i, sf);
+        }
+
+        Thread.sleep(5000);
+
+        for (int i = 0; i < 5; i++) {
+            ScheduledFuture<?> sf = futures.get(i);
+            if (sf.isCancelled())
+                System.out.println("thread " + i + " is cancelled");
+            else {
+                if (sf.cancel(true))
+                    System.out.println("thread " + i + " is cancelled right now");
+                else
+                    System.out.println("thread " + i + " is fail to cancel");
+            }
+
+            Thread.sleep(2000);
+        }
+
+        executor.shutdown();
+    }
 
 }
---
+++ src/main/java/com/zy/test/Test.java
@@ -0,0 +1,13 @@
+package com.zy.test;
+
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.print("Hello world");
+        test();
+    }
+
+    public static void test() {
+        System.out.print("test");
+    }
+}
